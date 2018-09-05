e8da4005f0e60955498397dcbe5283a2a1742962
AKNishantha <31471714+AKNishantha@users.noreply.github.com>
2018-09-05 17:47:42 +0530

    index.js updated with DOM concepts

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+
+ var config = {
+    apiKey: "AIzaSyBA3DgWVLxid0dw9UhoZgOneyjOu4zVsHE",
+    authDomain: "dimension-d27f1.firebaseapp.com",
+    databaseURL: "https://dimension-d27f1.firebaseio.com",
+    projectId: "dimension-d27f1",
+    storageBucket: "",
+    messagingSenderId: "708529966649"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
 
     document.getElementById("user_div").style.display = "block";
+	document.getElementById("selection_panel").style.display = "block";
+	document.getElementById("titlebar").style.display = "block";
     document.getElementById("login_div").style.display = "none";
-
+	document.getElementById("logo").style.display = "none"; 
     var user = firebase.auth().currentUser;
 
     if(user != null){
@@ -18,10 +35,13 @@ firebase.auth().onAuthStateChanged(function(user) {
     // No user is signed in.
 
     document.getElementById("user_div").style.display = "none";
+	document.getElementById("selection_panel").style.display = "none";
+	document.getElementById("titlebar").style.display = "none";
     document.getElementById("login_div").style.display = "block";
-
+	document.getElementById("logo").style.display = "block";
   }
 });
+
 
 function login(){
 
